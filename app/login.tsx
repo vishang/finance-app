0cd4dfc63ba67a37c24a3bb273b4335203f55098
Vishang Jad <vishang@idlvr.com.au>
2025-10-27 13:49:22 +0530

    nativewind integration

---
+++ app/login.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+
+const LoginPage = () => {
+
+    return (
+        <View className='flex-1 justify-center items-center bg-red-300'>
+            <Text>Hello There</Text>
+        </View>
+    )
+}
+
+export default LoginPage;
